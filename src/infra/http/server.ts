3e7e615f0495ac095cc8b32a364375b65cec25b7
Renan Silva <renaneduardo1@hotmail.com>
2025-06-17 21:29:32 -0300

    set global error handling

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,33 @@
 import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+	hasZodFastifySchemaValidationErrors,
+	serializerCompiler,
+	validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+	if (hasZodFastifySchemaValidationErrors(error)) {
+		reply.status(400).send({
+			message: 'Validation error',
+			errors: error.validation,
+		})
+	}
+
+	console.error(error)
+	return reply.status(500).send({
+		message: 'Internal server error',
+	})
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env.DATABASE_URL)
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
 	console.log('Server is running')
 })
