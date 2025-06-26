c087c82532cef47e22f62926fccc8edade353dc2
Renan Silva <renaneduardo1@hotmail.com>
2025-06-26 19:34:46 -0300

    add error state to Input

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,62 +1,70 @@
-import { CopyIcon } from '@phosphor-icons/react';
-import brevlyLogo from '../assets/Logo.svg'
-import { Button } from './components/ui/button';
-import { Input } from './components/ui/input';
+import { CopyIcon } from "@phosphor-icons/react";
+import brevlyLogo from "../assets/Logo.svg";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { useState } from "react";
 
 export function App() {
+	const [originalLinkInput, setOriginalLinkInput] = useState("");
+	const [aliasInput, setAliasInputInput] = useState("");
 
-  return ( 
-    <div className="min-h-screen flex flex-col items-center md:items-start md:pl-3">
-    {/* Logo */}
-    <div className="h-[25px] w-[96px] mb-[25px] mt-8 ">
-      <img src={brevlyLogo} alt="Brevly logo" />
-    </div>
+	return (
+		<div className="min-h-screen flex flex-col items-center md:items-start md:pl-3">
+			{/* Logo */}
+			<div className="h-[25px] w-[96px] mb-[25px] mt-8 ">
+				<img src={brevlyLogo} alt="Brevly logo" />
+			</div>
 
-    {/* Content: Form + List */}
-    <div className="w-full flex flex-col md:flex-row gap-3 md:gap-8 px-3 md:px-0">
-      {/* Form Area */}
-      <div className="w-full md:w-1/3 md:mb-0 bg-gray-100 rounded-lg">
-        <form className="bg-white p-4 shadow rounded space-y-4">
-          <div>
-            <Input id='original-link'type="text" label="Link Original" placeholder='link original'
-            />
-          </div>
-          <div>
-            <Input id='shortened-link'type="text" label="Link Encurtado"
-            
-            />
-          </div>
-          <Button
-          >
-            Salvar link
-          </Button>
-          <Button size="secondary">
-            <CopyIcon size={16}/>
-            Copiar
-          </Button>
-          <br />
-          <Button size="icon" >
-            <CopyIcon size={16}/>
-          </Button>
-        </form>
-      </div>
+			{/* Content: Form + List */}
+			<div className="w-full flex flex-col md:flex-row gap-3 md:gap-8 px-3 md:px-0">
+				{/* Form Area */}
+				<div className="w-full md:w-1/3 md:mb-0 bg-gray-100 rounded-lg">
+					<form className="bg-white p-4 shadow rounded space-y-4">
+						<div>
+							<Input
+								id="original-link"
+								type="text"
+								label="Link Original"
+								placeholder="link original"
+								value={originalLinkInput}
+								onChange={(e) => setOriginalLinkInput(e.target.value)}
+							/>
+						</div>
+						<div>
+							<Input
+								id="shortened-link"
+								type="text"
+								label="Link Encurtado"
+								error={true}
+								errorMsg="Valor inválido"
+								value={aliasInput}
+								onChange={(e) => setAliasInputInput(e.target.value)}
+							/>
+						</div>
+						<Button>Salvar link</Button>
+						<Button size="secondary">
+							<CopyIcon size={16} />
+							Copiar
+						</Button>
+						<br />
+						<Button size="icon">
+							<CopyIcon size={16} />
+						</Button>
+					</form>
+				</div>
 
-      {/* List Area */}
-      <div className="w-full md:w-2/3 bg-gray-100 rounded-lg">
-        <div className="bg-white p-4 shadow rounded space-y-2">
-          <h2 className="text-xl font-semibold mb-2">Meus Links</h2>
-          <ul className="list-disc list-inside space-y-1">
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+				{/* List Area */}
+				<div className="w-full md:w-2/3 bg-gray-100 rounded-lg">
+					<div className="bg-white p-4 shadow rounded space-y-2">
+						<h2 className="text-xl font-semibold mb-2">Meus Links</h2>
+						<ul className="list-disc list-inside space-y-1">
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
-  
-
-
-
--- web/src/components/ui/input.tsx
+++ web/src/components/ui/input.tsx
@@ -1,24 +1,35 @@
+import { WarningIcon } from "@phosphor-icons/react";
 import type { ComponentProps } from "react";
 
 type InputProps = ComponentProps<"input"> & {
 	label: string;
+	error?: boolean;
+	errorMsg?: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export function Input(props: InputProps) {
-	const { id, label } = props;
+	const { id, label, error, errorMsg, onChange } = props;
 
 	return (
-		<>
-			<label
-				htmlFor={id}
-				className="flex flex-col items-start gap-2 font-brevly text-xs font-normal uppercase text-gray-500 has-[input:focus]:text-blue-base"
-			>
-				<span> {label}</span>
+		<div className="flex flex-col gap-2 group" data-error={error}>
+			<label htmlFor={id} className="flex flex-col items-start gap-2 ">
+				<span className="font-brevly text-xs font-normal uppercase text-gray-500 has-[input:focus]:text-blue-base has-[input:focus]:font-bold group-data-[error=true]:text-danger">
+					{" "}
+					{label}
+				</span>
 				<input
-					className={`w-full h-12 py-0 px-4 justify-center gap-2 items-center rounded-lg border border-gray-300 overflow-hidden text-gray-400 text-ellipsis text-md font-normal focus:outline-[1.5px] focus:outline-blue-base focus:caret-blue-base`}
+					className={`w-full h-12 py-0 px-4 justify-center gap-2 items-center rounded-lg border border-gray-300 overflow-hidden text-gray-400 text-ellipsis text-md font-normal focus:outline-[1.5px] focus:outline-blue-base focus:caret-blue-base group-data-[error=true]:border-danger`}
 					{...props}
+					onChange={onChange}
 				/>
 			</label>
-		</>
+			{error && (
+				<span className="inline-flex gap-2 font-brevly items-center text-sm text-gray-500 font-normal">
+					<WarningIcon size={16} className="text-danger" />
+					{errorMsg}
+				</span>
+			)}
+		</div>
 	);
 }
