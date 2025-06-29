27a515c1a63ab46d071aba1af0ba685e03296bc9
Renan Silva <renaneduardo1@hotmail.com>
2025-06-29 17:47:58 -0300

    add variant support to FeedbackToast

--- web/src/components/ui/feedback-toast.tsx
+++ web/src/components/ui/feedback-toast.tsx
@@ -1,7 +1,22 @@
 import * as Toast from '@radix-ui/react-toast'
+import { tv, type VariantProps } from 'tailwind-variants'
 
-type ToastProps = {
-	type: 'success' | 'error' | 'info'
+const toastVariants = tv({
+	base: 'fixed bottom-0 right-0 z-50 flex flex-col items-start p-4 border rounded m-4',
+
+	variants: {
+		type: {
+			error: 'bg-danger-light border-danger text-danger',
+			info: 'bg-blue-light border-blue-base text-gray-100',
+		},
+	},
+
+	defaultVariants: {
+		type: 'info',
+	},
+})
+
+type ToastProps = VariantProps<typeof toastVariants> & {
 	title: string
 	message: string
 	duration?: number
@@ -9,15 +24,16 @@ type ToastProps = {
 	setOpen: (open: boolean) => void
 }
 export function FeedbackToast(props: ToastProps) {
-	const { title, message, duration = 3000, setOpen, isOpen } = props
+	const { title, message, duration = Infinity, setOpen, isOpen } = props
+
 	return (
 		<Toast.Provider duration={duration ? duration : 3000}>
 			<Toast.Root
 				open={isOpen}
 				onOpenChange={setOpen}
-				className="fixed bottom-0 right-0 z-50 flex flex-col items-start p-4 bg-danger-light border border-danger rounded text-danger"
+				className={toastVariants({ type: props.type })}
 			>
-				<Toast.Title className="font-bold text-danger">{title}</Toast.Title>
+				<Toast.Title className="font-bold">{title}</Toast.Title>
 				<Toast.Description>{message}</Toast.Description>
 			</Toast.Root>
 
