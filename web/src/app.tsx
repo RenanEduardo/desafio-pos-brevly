f86fd5c276778ae066a2127e211f979804838818
Renan Silva <renaneduardo1@hotmail.com>
2025-07-02 21:30:35 -0300

    Move feedback toast to app to cover all pages

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { FeedbackToast } from './components/ui/feedback-toast'
 import { Home } from './pages/home'
 import { NotFound } from './pages/not-found'
 import { RedirectPage } from './pages/redirect-page'
+import { useLinksStore } from './store/links'
 
 export function App() {
+	const { toast, setToastOpen } = useLinksStore((state) => state)
+
 	return (
 		<BrowserRouter>
 			<div>
+				<FeedbackToast
+					setOpen={setToastOpen}
+					isOpen={toast.isOpen}
+					title={toast.title}
+					message={toast.message}
+					type={toast.type}
+				/>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/:shortLink" element={<RedirectPage />} />
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,26 +1,12 @@
 import { AddNewLink } from '../components/add-link/add-new-link'
 import { LinksList } from '../components/link-list/links-list'
-import { FeedbackToast } from '../components/ui/feedback-toast'
 import { Header } from '../components/ui/header'
-import { useLinksStore } from '../store/links'
 
 export function Home() {
-	const { toast, setToastOpen } = useLinksStore((state) => state)
-
 	return (
 		<div className="min-h-screen flex md:items-center md:justify-center px-3 py-6 md:py-0">
-			<FeedbackToast
-				setOpen={setToastOpen}
-				isOpen={toast.isOpen}
-				title={toast.title}
-				message={toast.message}
-				type={toast.type}
-			/>
 			<div className="grid gap-6 w-full md:max-w-screen-lg">
-				{/* Header */}
 				<Header />
-
-				{/* Content Area */}
 				<div className="flex flex-col md:flex-row md:items-start md:gap-6 w-full gap-3">
 					<AddNewLink />
 					<LinksList />
